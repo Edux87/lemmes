16a93ae6ea0dcb8490acc1005ca6eeecea12cb20
Edgar <edaniel15@gmail.com>
2016-10-19 14:40:32 -0500

    add setup & lemma sentences

--- demo.py
+++ demo.py
@@ -3,8 +3,21 @@ from lemmes import Lemmatizer
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 if __name__ == '__main__':
   L = Lemmatizer()
-  # L.create_lemmas()
-  print(repr(L.lemmatize_sent(u'en los almacenes del callao a continuacion se detallan articulo', merge=True)))
+  print(repr(L.lemmatize_sent(u'Hola Bienvenidos a Lemmes!, diviértete', merge=True)))
+  print(repr(L.lemmatize_sent(u'hallan desechos hospitalarios en botadero', merge=True)))
+  print(repr(L.lemmatize_sent(u'ahora tu estas conmigo asi que vivamos el presentee', merge=True)))
+  print(repr(L.lemmatize_sent(u'los borraste o los descargaste a un .zip y backup? se pueden bajar carpetas independientes del gmail automáticamente.', merge=True)))
+  print(repr(L.lemmatize(u'extraordinarios')))
+  print(repr(L.lemmatize(u'extraterritoriales')))
+  print(repr(L.lemmatize(u'extrañezas')))
+  print(repr(L.lemmatize(u'extorsiones')))
+  print(repr(L.lemmatize(u'extorsionadores')))
+  print(repr(L.lemmatize(u'extenuantemente')))
+  print(repr(L.lemmatize(u'ayen')))
+  print(repr(L.lemmatize(u'hallar')))
+  print(repr(L.lemmatize(u'hall')))
+  print(repr(L.lemmatize(u'causita')))
+  print(repr(L.lemmatize(u'borrachines')))
--- setup.py
+++ setup.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import re
 from setuptools import setup, find_packages
+from lemmes import Lemmatizer
+
 
 def find_version(fname):
-    """Attempts to find the version number in the file names fname.
-    Raises RuntimeError if not found.
-    """
-    version = ''
-    with open(fname, 'r') as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
+  """Attempts to find the version number in the file names fname.
+  Raises RuntimeError if not found.
+  """
+  version = ''
+  with open(fname, 'r') as fp:
+    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    for line in fp:
+      m = reg.match(line)
+      if m:
+        version = m.group(1)
+        break
+  if not version:
+    raise RuntimeError('Cannot find version information')
+  return version
 
-__version__ = find_version("lemmes/__init__.py")
-
-REQUIREMENTS = [
+if __name__ == '__main__':
+  __version__ = find_version("lemmes/__init__.py")
+  REQUIREMENTS = [
     'nltk',
     'numpy',
-    'scipy',
-    'scikit-learn',
-    'Cython',
-    'simplejson',
-    'python-slugify'
-]
-
-DESCRIPTION = ("Compact interface for Assisted Machine Learning")
-with open('README.md') as f:
+    'backports.csv'
+  ]
+  DESCRIPTION = ("Interface for Lemmas in Spanish")
+  with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
-setup(
-    name='Jiazz',
+  setup(
+    name='Lemmes',
     version=__version__,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
@@ -49,12 +45,15 @@ setup(
     install_requires=REQUIREMENTS,
     tests_require=['testfixtures'],
     classifiers=[
-        'Development Status :: 1.0',
-        'Environment :: Console',
-        'License :: NonFree',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: NTL :: Lemmas :: Spanish',
+      'Development Status :: 1.0',
+      'Environment :: Console',
+      'License :: NonFree',
+      'Intended Audience :: Developers',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: NTL :: Lemmas :: Spanish',
     ],
-)
+  )
+
+  L = Lemmatizer()
+  print(repr(L.lemmatize_sent(u'Hola Bienvenidos a Lemmes!, diviértete', merge=True)))
